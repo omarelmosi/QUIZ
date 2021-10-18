57d6d518e979aac77518075ef47d008097e2c5e7
omarelmosi <63499128+omarelmosi@users.noreply.github.com>
2021-10-18 14:17:29 +0300

    Update end.js

--- quiz app/end.js
+++ quiz app/end.js
@@ -1,12 +1,12 @@
 let congrat = document.getElementById("congrat")
 let btn = document.querySelector("button")
 let h4 = document.querySelector("h4")
-if(localStorage.getItem("score") < 10){
+if(localStorage.getItem("score") < 21){
     congrat.textContent = "سقطت يا فالح"
 }else{
     congrat.textContent = "الله يباركلك يبني"
 }
-h4.textContent = `النتيجة ${localStorage.getItem("score")} من 21`
+h4.textContent = `النتيجة ${localStorage.getItem("score")} من 42`
 btn.onclick = function(e){
     location.assign("./start.html")
 }
